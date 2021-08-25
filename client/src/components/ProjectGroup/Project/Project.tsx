8ead413080d60b90ca61821b62d3ccdb9bb71318
Alfie <4998293+AlfieGoldson@users.noreply.github.com>
2021-08-26 01:45:59 +0200

    project buttons icon

--- client/src/components/ProjectGroup/Project/Project.tsx
+++ client/src/components/ProjectGroup/Project/Project.tsx
@@ -3,6 +3,7 @@ import { Project as Props } from '~data/projects';
 import { TagGroup } from '~components/TagGroup';
 import { Button } from '~components/Button';
 import Image from 'next/image';
+import { techIcons } from '~util/icons';
 
 export const Project = ({
 	name,
@@ -40,6 +41,14 @@ export const Project = ({
 			<div className={styles.ctaGroup}>
 				{link && (
 					<Button href={link} style='Primary' external>
+						<svg
+							width='24'
+							height='24'
+							viewBox='0 0 24 24'
+							xmlns='http://www.w3.org/2000/svg'
+						>
+							<path d='M17.001 20H6.00101C5.47057 20 4.96187 19.7893 4.58679 19.4142C4.21172 19.0391 4.00101 18.5304 4.00101 18V7C4.00101 6.46957 4.21172 5.96086 4.58679 5.58579C4.96187 5.21071 5.47057 5 6.00101 5H10.001V7H6.00101V18H17.001V14H19.001V18C19.001 18.5304 18.7903 19.0391 18.4152 19.4142C18.0401 19.7893 17.5314 20 17.001 20ZM11.701 13.707L10.291 12.293L16.584 6H13.001V4H20.001V11H18.001V7.415L11.701 13.707Z' />
+						</svg>
 						Visit
 					</Button>
 				)}
